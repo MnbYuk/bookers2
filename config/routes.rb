0f5f5657ece38f9a80d7673a7ab58b46aa3443cb
MnbYuk <haiiroorennji@gmail.com>
2024-04-09 13:41:03 +0000

    aboutページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  
+  get "homes/about" => "homes#about", as: "about"
 
   resources :books, only: [:index, :new, :show]
   resources :users, only: [:show, :edit]
