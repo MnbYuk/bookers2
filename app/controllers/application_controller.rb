6a5b0db803695a2305f2a68fe2aa75cea1ccf69a
MnbYuk <haiiroorennji@gmail.com>
2024-04-26 05:42:34 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configiure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(@user.id)
+    @user = current_user.id
+    user_path(@user)
   end
 
   def after_sign_out_path_for(resource)
